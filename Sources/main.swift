550a1978264450b313d0a9e04a3398c022296cf5
rolivieri <roliv@us.ibm.com>
2016-12-09 12:51:06 -0600

    test

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,20 @@
+class MyClass {
+  let myStr = "a string"
+  init() {
+    print("In initializer()...")
+  }
+
+  func xyz() {
+    print("In xyz()... \(myStr)")
+  }
+
+}
 
 var counter = 0
+let myObj = MyClass()
 while (true) {
   counter += 1
   print("Hello, world!")
+  myObj.xyz()
   print("counter: \(counter)")
 }
